dbb140ca44126240551ab1864473a34a33f44efa
Subash-Kumar-8 <subashkumar123ak@gmail.com>
2026-05-04 13:35:31 +0530

    Fixed case-sensitive errors

--- client/src/profile/page.jsx
+++ client/src/profile/page.jsx
@@ -1,5 +1,5 @@
 import { useNavigate } from "react-router-dom";
-import { useAuth } from "../context/AuthContext";
+import { useAuth } from "../context/authContext";
 import { useState } from "react";
 
 const Profile = () => {
